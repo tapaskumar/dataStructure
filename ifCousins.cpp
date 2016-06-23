639d8084ed380b351c9b2abad8b2c25247092d1f
tapas3loq <tapas@3loq.com>
2016-06-23 14:49:26 +0530

    check if two nodes are cousins in a tree

--- ifCousins.cpp
+++ ifCousins.cpp
@@ -13,22 +13,30 @@ node* left;
 node* right;
 };
 
-bool sameparent()
+bool sameparent(node* root,node* n1,node* n2)
 {
+if (root->left == NULL || root->right == NULL)
+	return false;	
 
+if(root->left == n1 && root->right == n2)
+	return true;
+	
+if( sameparent(root->left,n1,n2) || sameparent(root->right,n1,n2) )
+	return true;
+		
 }
 
 int getHeight(node* root,node* n,int height)
 {
 
-if(root->data == n->data)
-return height;	
-
 if(root == NULL)
 return 0;
 
-h1 = getHeight(root->left,n,height+1);
-h2 = getHeight(root->right,n,height+1);	
+if(root->data == n->data)
+return height;	
+
+int h1 = getHeight(root->left,n,height+1);
+int h2 = getHeight(root->right,n,height+1);	
 
 return h1 + h2;
 
@@ -66,11 +74,14 @@ int main()
     node *root        = newNode(20);
     root->left               = newNode(8);
     root->right              = newNode(22);
+    root->right->left        = newNode(11);
     root->left->left         = newNode(4);
     root->left->right        = newNode(12);
     root->left->right->left  = newNode(10);
     root->left->right->right = newNode(14);
 
+	if ( cousins(root,root->right->left,root->left->right) )
+		cout<<" they are cousins "<<endl;
 
 
 return 0;
